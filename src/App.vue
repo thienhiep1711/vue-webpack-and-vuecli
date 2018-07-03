7f2694ab38f252a8f0a54aafe976fcb2f0888256
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2018-07-03 16:49:54 +0700

    Done chap 9

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,20 @@
         </div>
          <div class="col-sm-9">
            <div class="main-box">
-             <app-user></app-user>
+             <!-- <app-user></app-user> -->
+             <div class="text-center">
+               <button class="btn btn-success" @click="selectedComponent = 'appQuote'">News</button>
+               <button class="btn btn-warning"  @click="selectedComponent = 'quoteAuthor'">Author</button>
+               <button class="btn btn-primary"  @click="selectedComponent = 'quoteNew'">Quote</button>
+             </div>
+             <p>{{selectedComponent}}</p>
+             <component :is="selectedComponent">
+               <p>Default Content</p>
+             </component>
+             <!-- <app-quote :quote="'Hiep This is wonderful quote'">
+               <h2 slot="title">{{quoteTitle}}</h2>
+              <p slot="content">Nguyen Hiep recomentdation </p>
+             </app-quote> -->
         <!-- <app-servers></app-servers> -->
            </div>
       </div>
@@ -27,16 +40,29 @@
   import Sidebar from './components/Sidebar/Sidebar';
   import User from './components/Users/User';
   import ServerStatus from './components/Server/ServerStatus';
+  import Quote from './components/Quote/Quote.vue';
+
+  import Author from './components/Quote/Author.vue';
+  import New from './components/Quote/New.vue';
 
   import './assets/css/bootstrap.min.css';
   import './assets/css/styles.scss';
   export default {
+    data: function() {
+      return {
+        quoteTitle:"The Quote",
+        selectedComponent:'appQuote'
+      }
+    },
     components: {
       'app-header': Header,
       'app-footer': Footer,
       'app-sidebar': Sidebar,
       'app-user': User,
       'app-server-status': ServerStatus,
+      'appQuote': Quote,
+      'quoteNew': New,
+      'quoteAuthor' : Author,
     }
   }
 
---
+++ src/components/Quote/Quote.vue
@@ -0,0 +1,29 @@
+<template>
+   <div>
+     <div class="row">
+       <div class="col-12">
+         <div class="box-slot">
+            <slot name="title"></slot>
+            <h2>
+              <slot name="subtitle">The subtitle</slot>
+            </h2>
+
+         </div>
+
+       </div>
+        <div class="col-12">
+          <div class="box-slot">
+              <slot></slot>
+
+          </div>
+       </div>
+     </div>
+   </div>
+
+</template>
+<script>
+export default {
+  props: ['quote']
+}
+
+</script>
