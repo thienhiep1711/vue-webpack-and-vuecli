954cf2532eb9232ab42b7944ebb01f1ec5778495
Nguyen Hiep <thienhiep171193@gmail.com>
2018-07-08 17:54:51 +0700

    Chapter 15 : Connecting to Servers via Http - Using vue-resource
    https://www.safaribooksonline.com/videos/vue-js-2/9781788992817/9781788992817-video15_13

--- src/components/Forms/ProfileForm.vue
+++ src/components/Forms/ProfileForm.vue
@@ -6,31 +6,31 @@
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-10">
-              <input type="email" class="form-control" placeholder="Email" :value="userData.email" @input="userData.email = $event.target.value">
+              <input type="email" class="form-control" placeholder="Email" :value="user.userData.email" @input="user.userData.email = $event.target.value">
             </div>
           </div>
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Password</label>
             <div class="col-sm-10">
-              <input type="password" class="form-control" placeholder="Password" v-model="userData.password">
+              <input type="password" class="form-control" placeholder="Password" v-model="user.userData.password">
             </div>
           </div>
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Name</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" placeholder="Name" v-model="userData.name">
+              <input type="text" class="form-control" placeholder="Name" v-model="user.userData.name">
             </div>
           </div>
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Age</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" placeholder="Name" v-model="userData.age">
+              <input type="text" class="form-control" placeholder="Name" v-model="user.userData.age">
             </div>
           </div>
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Message</label>
             <div class="col-sm-10">
-              <textarea rows="3" class="form-control" placeholder="Message" v-model="message">
+              <textarea rows="3" class="form-control" placeholder="Message" v-model="user.message">
               </textarea>
             </div>
           </div>
@@ -39,13 +39,13 @@
               <legend class="col-form-label col-sm-2 pt-0">Gender</legend>
               <div class="col-sm-10">
                 <div class="form-check">
-                  <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="Male" checked v-model="gender">
+                  <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="Male" checked v-model="user.gender">
                   <label class="form-check-label" for="gridRadios1">
                     Male
                   </label>
                 </div>
                 <div class="form-check">
-                  <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="Female" v-model="gender">
+                  <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="Female" v-model="user.gender">
                   <label class="form-check-label" for="gridRadios2">
                     Female
                   </label>
@@ -57,13 +57,13 @@
             <div class="col-sm-2">Major</div>
             <div class="col-sm-10">
               <div class="form-check">
-                <input class="form-check-input" type="checkbox" v-model="major" value="Engginer">
+                <input class="form-check-input" type="checkbox" v-model="user.major" value="Engginer">
                 <label class="form-check-label" for="gridCheck1">
                   Engginer
                 </label>
               </div>
               <div class="form-check">
-                <input class="form-check-input" type="checkbox"  v-model="major" value="ComputerSience">
+                <input class="form-check-input" type="checkbox" v-model="user.major" value="ComputerSience">
                 <label class="form-check-label" for="gridCheck1">
                   Computer Sience
                 </label>
@@ -71,90 +71,111 @@
             </div>
           </div>
           <div class="form-group row">
-            <div class="col-sm-2">Salary</div>
-            <div class="col-sm-10">
-              <select name="" id="" class="form-control" v-model="selectedSalarys">
-                <option :value="salary" v-for="(salary, index) in salarys" :key="index"> {{salary}}</option>
-              </select>
-            </div>
-          </div>
-           <div class="form-group row">
             <div class="col-sm-2">Active</div>
             <div class="col-sm-10">
-              <app-switch v-model="dataSwitch"></app-switch>
+              <app-switch v-model="user.dataSwitch"></app-switch>
             </div>
           </div>
           <div class="form-group row">
             <div class="col-sm-10 col-ofset-sm-2">
-              <button type="submit" @click.prevent="submited" class="btn btn-primary">Sign in</button>
+              <button type="submit" @click.prevent="submited" class="btn btn-primary">Submit</button>
             </div>
           </div>
         </form>
+
       </div>
 
-      <div class="col-sm-5" v-if="isSubmited">
+      <div class="col-sm-5" v-if="user.isSubmited">
         <div class="alert alert-success" role="alert">
-         Submit Successfull
+          Submit Successfull
         </div>
       </div>
-      <div class="col-sm-5" v-if="!isSubmited">
+      <div class="col-sm-5" v-if="!user.isSubmited">
         <app-message :backgroundHighlight="{mainColor:'red', secondColor:'blue', delay:'4000'}"></app-message>
-        <app-message :backgroundHighlight="{mainColor:'blue', secondColor:'green', delay:'3000'}"></app-message>
-        <app-message :backgroundHighlight="{mainColor:'green', secondColor:'pink', delay:'2000'}"></app-message>
         <h3>Infomation</h3>
         <ul>
           <li>
-            <strong>Email:</strong> {{userData.email}}</li>
+            <strong>Email:</strong> {{user.userData.email}}</li>
           <li>
-            <strong>Password:</strong> {{userData.password}}</li>
+            <strong>Password:</strong> {{user.userData.password}}</li>
           <li>
-            <strong>Name:</strong> {{userData.name}}</li>
+            <strong>Name:</strong> {{user.userData.name}}</li>
           <li>
-            <strong>Age:</strong> {{userData.age}}</li>
+            <strong>Age:</strong> {{user.userData.age}}</li>
           <li>
-            <strong>Message:</strong> {{message}}</li>
+            <strong>Message:</strong> {{user.message}}</li>
           <li>
-            <strong>Gender:</strong> {{gender}}
+            <strong>Gender:</strong> {{user.gender}}
           </li>
           <li>
-            <strong>Major:</strong>  <span v-for="(item, index) in major" :key="index">{{item}}</span>
+            <strong>Major:</strong>
+            <span v-for="(item, index) in user.major" :key="index">{{item}}</span>
           </li>
           <li>
             <strong>Swiched</strong>
-            {{selectedSalarys}}
+            {{user.selectedSalarys}}
           </li>
           <li>
             <strong>Swiched Active</strong>
-            {{dataSwitch}}
+            {{user.dataSwitch}}
           </li>
           <li>
             <strong>Submit</strong>
           </li>
         </ul>
       </div>
+      <div class="col-sm-12">
+        <input type="text" class="form-control" v-model="node">
+        <button class="btn btn-success" @click="fetchData">Load data</button>
+        <br/>
+        <table class="table table-table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(u, index) in users" :key="index">
+              <td>{{u.userData.email}}</td>
+              <td>{{u.userData.name}}</td>
+              <td>{{u.userData.age}}</td>
+              <td>{{u.gender}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 
 </template>
 <script>
-import SwitchButton from './SwitchButton.vue';
-import MessageForm from './MessageForm';
+  import SwitchButton from './SwitchButton.vue';
+  import MessageForm from './MessageForm';
   export default {
     data() {
       return {
-        userData: {
-          email: '',
-          password: '',
-          name: '',
-          age: 27
+        user: {
+          userData: {
+            email: '',
+            password: '',
+            name: '',
+            age: 27
+          },
+          message: "",
+          gender: 'Male',
+          major: [],
+          selectedSalarys: '20000',
+          dataSwitch: true,
+          isSubmited: false
         },
-        message: "",
-        gender: 'Male',
-        major: [],
-        salarys: ['10000' , '20000', '40000', '80000'],
-        selectedSalarys: '20000',
-        dataSwitch:true,
-        isSubmited:false
+        users: [],
+        resurce: {
+
+        },
+        node:'data'
       }
     },
     components: {
@@ -163,8 +184,30 @@ import MessageForm from './MessageForm';
     },
     methods: {
       submited() {
-        this.isSubmited = true
+        this.user.isSubmited = true
+        // this.resurce.save({}, this.user);
+        this.resurce.saveCustomer(this.user);
+      },
+      fetchData() {
+      this.resurce.getData({node: this.node})
+           .then(response => {
+            return response.json()
+          })
+          .then(data => {
+            const resultArray = [];
+            for (let key in data) {
+              resultArray.push(data[key]);
+            }
+            this.users = resultArray;
+          })
       }
+    },
+    created() {
+      const customActions = {
+          saveCustomer: {method:'POST', url: 'customer.json'},
+          getData: {method:'GET'}
+      }
+      this.resurce = this.$resource('{node}.json', {}, customActions)
     }
   }
 
@@ -174,11 +217,9 @@ import MessageForm from './MessageForm';
   ul {
     list-style-type: none;
     padding: 0;
-
   }
 
   ul li {
     padding: 5px 0;
   }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Home from './components/Home.vue'
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-http-hiep.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if(request.method == 'POST') {
+    request.method = 'PUT'
+  }
+  next(response => {
+    response.json = () => {
+      return {
+        messages: response.body
+      }
+    }
+  });
+})
 
 Vue.component('app-servers' , Home)
 export const eventBus = new Vue({
@@ -10,11 +28,10 @@ export const eventBus = new Vue({
     }
   }
 });
+
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
 })
-
-
-
-
